060e7de6aa2f1eddf32d39fe069f271cb200dc33
Carsten Daurehøj <arelstone@gmail.com>
2021-09-23 12:58:08 +0200

    🔖  Bump version 1.0.6

---
+++ scripts/post-version.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+PACKAGE_VERSION=$(cat package.json | grep \\\"version\\\" | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') 
+
+yarn install
+
+echo 'Generating typings'
+yarn build
+
+echo "🔖 Creating tag for \"$PACKAGE_VERSION\""
+git add .
+git commit -m "🔖 Bump version /"$PACKAGE_VERSION/""
+git tag $PACKAGE_VERSION 
+git push --tags
+git push
